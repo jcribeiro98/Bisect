da11f222e2f98c877f94fa7d1ea0960e36874873
jose <jose.cribeiro@kit.edu>
2022-09-01 18:11:50 +0200

    Added the function that performs the inference. Currently, the process of fitting a model has a placeholder value on it until a proper fit function is coded

---
+++ src/inference_methods.R
@@ -0,0 +1,31 @@
+library(sets)
+library(glue)
+library(dplyr)
+source("src/utils.R")
+
+
+inference <- function(x,S, method = "mahalanobis"){
+  
+  if(exists("ODM_env", envir = globalenv()) != T){
+    for (s in set_power(as.numeric(1:(ncol(DB)-2)))){
+      if (set_is_empty(s) != T){
+        
+        ODM_env[[glue("method{set_names(s)}")]] = 2#' Here we are going to add 
+                                                   #' the call of a new function
+                                                   #' that fits the desired 
+                                                   #' method to the subspace.
+        ODM_env <<-ODM_env
+        rm(ODM_env,envir = enviroment())
+      }
+    }
+  }
+  
+  #Methods: Given how each method is store in the method environment, call them
+  #to perform inference 
+  
+  if(method == "mahalanobis"){
+    result = ODM_env[[glue("method{set_names(S)}")]]
+  }
+  
+  return(result)
+}
