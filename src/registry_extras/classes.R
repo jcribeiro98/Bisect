333aa3b1636656c4fea53db7ab361ce5aa36c8bf
jose <jose.cribeiro@kit.edu>
2022-09-06 20:54:26 +0200

    Changed the summary function of the class for a different formatting

--- src/registry_extras/classes.R
+++ src/registry_extras/classes.R
@@ -11,7 +11,7 @@ hog_method <- function(DB, B ,od_name, hog_name,
 }
 
 print.hog_method <- function(method){
-  print(glue("{bold('Hidden Outlier Generation Method Oject')}\n\n
+  print(glue("{bold('Hidden Outlier Generation Method Object')}\n\n
              Outlier Gen Method used: {method$odm_name}\n
              Synthetic HO generation method employed: {method$hog_name}.\n\n
              {cyan('Use summary() for a detailed description')}\n
@@ -19,22 +19,26 @@ print.hog_method <- function(method){
              enviroment ODM_env (<your_method>$ODM_env).')}"))}
           
 summary.hog_method <- function(method){
-  print(glue("{bold('Hidden Outlier Generation Method Oject')}\n\n
+  print(glue("{bold('Hidden Outlier Generation Method Object')}\n\n
              Outlier detection method used: {method$odm_name}\n
              Synthetic HO generation method employed: {method$hog_name}.
              
-             \n\n Database summary:
-             \n
-             \t\t number of features: {ncol(method$DB)-2}
-             \t\t total number of data points: {nrow(method$DB)}
-             \t\t total amount of syntetic data generated: {method$n_data_gen}
-              \t\t\t ...of which hidden outliers: {nrow(method$ho_list)}
-             \t\t number of H1 outliers: {as.data.frame(method$ho_type) %>%
+\n\n {underline('Database summary')}:
+\n
+             * {italic('Number of features:')} {ncol(method$DB)-2}
+             * {italic('Total number of data points:')} {nrow(method$DB)}
+             * {italic('Total amount of syntetic data generated:')} {
+             method$n_data_gen}
+             \t          {italic('...of which hidden outliers:')} {nrow(
+             method$ho_list)}
+             * {italic('Number of H1 outliers')}: {
+              as.data.frame(method$ho_type) %>%
               filter(V1 == 'H1') %>%
               count()}
-             \t\t number of H2 outliers: {as.data.frame(method$ho_type) %>%
+             * {italic('Number of H2 outliers:')} {
+              as.data.frame(method$ho_type) %>%
               filter(V1 == 'H2') %>%
               count()}.
 
-             \n\n Total execution time: {method$exec_time}."))
+\n\n {cyan('Total execution time:')} {method$exec_time}."))
 }
