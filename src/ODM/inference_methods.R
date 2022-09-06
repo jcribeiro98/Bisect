785798b8b937cb376e9828a6a998b73e04c6c3fd
jose <jose.cribeiro@kit.edu>
2022-09-06 19:57:36 +0200

    Added some function descriptions

--- src/ODM/inference_methods.R
+++ src/ODM/inference_methods.R
@@ -6,14 +6,27 @@ source("src/ODM/fit_method.R")
 
 
 inference <- function(x,S, method = "mahalanobis",...){
-  ##Pending description##
+  #' @title Inference function
+  #' 
+  #' @description Manages the inference for each model.
+  #' Each model is fitted within an enviroment saved in the Glob env.
+  #' This function will manage how each method is going to be called 
+  #' in the enviroment to perform inference. If no method is fitted, 
+  #' the function will call the fit_all_methods function to fit all 
+  #' the method in all of the possible subspaces. 
+  #' 
+  #' Arguments:
+  #' @param x: Value to infere
+  #' @param S: Subspace to where perform the inferece
+  #' @param method: ODM
+  #' @param ...: Params passed to some ODMs.
   
   
   if(exists("ODM_env", envir = globalenv()) != T){
     fit_all_methods(method,...)
   }
   
-  #Methods: Given how each method is defined in the method environment, call them
+  #Methods:Given how each method is defined in the method environment, call them
   #to perform inference 
   
   if(method == "mahalanobis" ){
