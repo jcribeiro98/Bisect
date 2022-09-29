a885824c80865124cd88193b9f7e9b3fe63d7e37
jose <jose.cribeiro@kit.edu>
2022-09-29 15:31:40 +0200

    Removed the f function from bisect_strat and made the adaptation to the new function outlier_check

--- src/HOGen/bisect_strat.R
+++ src/HOGen/bisect_strat.R
@@ -8,6 +8,7 @@ library(tictoc)
 source("src/registry_extras/utils.R")
 source("src/ODM/inference_methods.R")
 source("src/registry_extras/classes.R")
+source("src/HOGen/otlier_check.R")
 
 
 bisect <- function(l, x, iternum = 1000, method, verb = TRUE, ...) {
@@ -36,7 +37,7 @@ bisect <- function(l, x, iternum = 1000, method, verb = TRUE, ...) {
   for (i in 1:iternum) {
     c <- (b + a) / 2
 
-    check_if_outlier <- f(c * x + colMeans(DB[2:(ncol(DB) - 1)]),
+    check_if_outlier <- outlier_check(c * x + colMeans(DB[2:(ncol(DB) - 1)]),
       method = method, verb = verb
     )
     outlier_indicator <- check_if_outlier[[1]]
@@ -57,64 +58,6 @@ bisect <- function(l, x, iternum = 1000, method, verb = TRUE, ...) {
 }
 
 
-f <- function(x, verb = F, method = "mahalanobis", ...) {
-  #' @title Bisection main function
-  #'
-  #' @description Defines the function that is going to be used in the bisection
-  #' rule during the main execution of the algorithm. The function itself is
-  #' really simple, it checks if a point x is an outlier in all of the subspaces
-  #' of the total space. f is defined as
-  #'
-  #'        1, if x is outside of bounds,
-  #' f(x)= -1, if x is in the join acceptance area,
-  #'        0, if x is in H1 U H2.
-  #'
-  #' @note The function only works currently with mahanalobis distances as an
-  #' outlier detection method (will probably remain like that in this file).
-  #'
-  #' Arguments:
-  #' @param x :(array) Point to check
-  #' @param verb : (Logical) Value controlling if f should be verbose or not (
-  #'               defaults to FALSE)
-
-
-
-  h2 <- matrix(0, ncol = 2^(ncol(DB) - 2) - 1, nrow = 1)
-  h2[2^(ncol(DB) - 2) - 1] <- 1
-
-  supS <- set_power(as.numeric(1:(ncol(DB) - 2)))
-  index <- matrix(0, ncol = 2^(ncol(DB) - 2) - 1, nrow = 1)
-
-  j <- 0
-  for (S in supS) {
-    if (set_is_empty(S) != T) {
-      if (inference(x, S, method, ...)) {
-        index[j] <- 1
-      }
-    }
-    j <- j + 1
-  }
-
-  if (sum(index[1:(2^(ncol(DB) - 2) - 2)]) > 0 &&
-    index[2^(ncol(DB) - 2) - 1] == 0) {
-    result <- list(0, "H1")
-  } else if (isTRUE(all.equal(index, h2))) {
-    result <- list(0, "H2")
-  } else if (sum(index[1:(2^(ncol(DB) - 2) - 2)]) > 0 &&
-    index[2^(ncol(DB) - 2) - 1] == 1) {
-    if (verb == T) {
-      print(glue("x Outside of bounds"))
-    }
-    result <- list(1, "OB")
-  } else {
-    if (verb == T) {
-      print(glue("x in the total acceptance area"))
-    }
-    result <- list(-1, "IL")
-  }
-
-  return(result)
-}
 
 
 main <- function(gen_points = 100, method = "mahalanobis", seed = FALSE,
